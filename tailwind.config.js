68fd207833bd8da586f32eda8ae4ead9ac66b0cf
iamnaveeth <navneethkotian17@gmail.com>
2021-06-23 07:48:35 +0530

    nothing changed

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        first:'#FF6363',
+        second:{
+          100:'#E2E2D5',
+          200:'#888883',
+          lg:'#f9f9f9',
+          shade:'#FAA71F'
+        }
+      },
+      fontFamily:{
+        gallery:['Big Shoulders Stencil Display'],
+        gall1:['Orbitron']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
